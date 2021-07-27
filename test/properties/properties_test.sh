7fcbf15002bdd1549aff2541cadc86f496c55fc1
adelehedde <87861638+adelehedde@users.noreply.github.com>
2021-07-27 17:59:29 +0200

    [Test] Updated wrong md5sum

--- test/properties/properties_test.sh
+++ test/properties/properties_test.sh
@@ -19,26 +19,31 @@ unit_test::after_all() {
 }
 
 test::should_get_property() {
+  local property
   property="$(properties::get "property" "$test_properties_file_path")"
   assert_test::equals "value" "$property"
 }
 
 test::should_get_correct_property() {
+  local property
   property="$(properties::get "property.key.2" "$test_properties_file_path")"
   assert_test::equals "value.2" "$property"
 }
 
 test::should_get_property_case_insensitive() {
+  local property
   property="$(properties::get "upper_property" "$test_properties_file_path" "true")"
   assert_test::equals "VALUE" "$property"
 }
 
 test::should_get_empty_value_when_property_does_not_exist() {
+  local property
   property="$(properties::get "666" "$test_properties_file_path")"
   assert_test::equals "" "$property"
 }
 
 test::should_get_required_property() {
+  local property
   property="$(properties::get_required "property" "$test_properties_file_path")"
   assert_test::equals "value" "$property"
 }
@@ -49,24 +54,24 @@ test::should_exit_when_property_is_not_present() {
 
 test::should_update_property() {
   properties::set "property.key.2"  "new value" "$test_properties_file_path"
-  assert_test::md5_equals "11ef1938e5db6d288f8f0bfeb5610d7b" "$test_properties_file_path"
+  assert_test::md5_equals "f643f910aeb4737868c4fdc2ab60b90f" "$test_properties_file_path"
 }
 
 test::should_add_property() {
   properties::set "property.key.4"  "value4" "$test_properties_file_path"
-  assert_test::md5_equals "3e7645ecdd80685db9249bab088f0e3f" "$test_properties_file_path"
+  assert_test::md5_equals "6ebc0255015f4c6c4f68a464de8470d5" "$test_properties_file_path"
 }
 
 test::should_update_empty_property() {
   local empty_value=""
   properties::set "property.key.1" "$empty_value" "$test_properties_file_path"
-  assert_test::md5_equals "5d29ae94a931d1da91972dc1165cc879" "$test_properties_file_path"
+  assert_test::md5_equals "c0cf1c21da158137c4e0547915f83ec6" "$test_properties_file_path"
 }
 
 test::should_add_empty_property() {
   local empty_value=""
   properties::set "property.key.4" "$empty_value" "$test_properties_file_path"
-  assert_test::md5_equals "af8acc944c2d0ff6df174a45214538a0" "$test_properties_file_path"
+  assert_test::md5_equals "d633d33fc3e9da363f74a4778fb5b68b" "$test_properties_file_path"
 }
 
 unit_test::run
